969f8a459b56979983c379686933eca3936d1e43
faydho <faydho>
2020-01-09 01:11:20 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum in odio dicta error deleniti, commodi sequi assumenda quia natus ex enim iusto culpa vitae eaque molestias aut quod, distinctio sapiente.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
